d104d8a8dc5d6519de8ece0c6353dbeea4d20b6b
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-01-09 19:56:54 +0530

    animate scroll down

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const dropdownToggle = document.querySelector(".dropdown-toggle");
 const dropdownList = document.querySelector(".dropdown-list");
 
 function toggle() {
-  dropdownList.classList.toggle("show");
+  const { maxHeight } = window.getComputedStyle(dropdownList);
+  if (maxHeight === "0px") {
+    dropdownList.style.maxHeight = `${dropdownList.scrollHeight}px`;
+  } else {
+    dropdownList.style.maxHeight = null;
+  }
 }
 
 dropdownToggle.addEventListener("click", toggle);
